f28b647ec84fb0027a337aa6d375b7b42d9d0a42
Elijah Reale <elijahreale@gmail.com>
2013-03-23 16:04:10 -0700

    Full Commit - Working

---
+++ js/widget.js
@@ -0,0 +1,50 @@
+/*      
+function overlap() {
+}
+*/      
+
+// use to save widget position
+function findPos(div) {
+  var id = $(div).attr("id");
+
+  var w = ($(div).width()).toFixed();
+  var h = ($(div).height()).toFixed();
+  
+  var position = $(div).position();
+  var x = (position.left).toFixed();
+  var y = (position.top).toFixed();
+  
+
+  //debug
+  $("#"+id).find("span:first").text( "L:" + x + ", T:" + y + ", W:" + w + ", H:" + h);
+}
+      
+// allow divs to collide with each other
+$(document).ready(function(){
+   
+  // everything that can be dragged around
+  var $draggables = $(".widget");
+  var id, $draggableItem;
+
+  // go through each item, get it's id,
+  // and tell draggable() to collide with every obstacle but itself
+  for (var i = 0; i < $draggables.length; i++) {
+    $draggableItem = $draggables.eq(i);
+    id = $draggableItem.attr("id");
+
+    $draggableItem.draggable({
+      handle: ".WidgetHeader",
+      //containment: "parent",																				 /* use 'restraint' and 'preventProtrusion' instead */
+      snap: true,
+      obstacle: ".widget[id!=\"" + id + "\"], #bumber",                /* temp solution */
+      preventCollision: true,
+      restraint: "#tabs-1",                                            /* temp solution */
+      preventProtrusion: true
+    });
+  }
+  
+  $( ".widget" ).resizable({
+    //stop: overlap,
+    autoHide: true,
+  });  
+});
